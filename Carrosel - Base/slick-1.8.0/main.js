2874fb14e9d04cf2d774f9b1cc300adbcf9154a1
douglas <dfcs@cin.ufpe.br>
2018-12-28 22:50:56 -0300

    Uptade

---
+++ Carrosel - Base/slick-1.8.0/main.js
@@ -0,0 +1,11 @@
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+
+$(document).ready(function(){
+  $('.slide').slick({
+    setting-name: setting-value
+  });
+});
